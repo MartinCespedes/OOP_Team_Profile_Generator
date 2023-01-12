e80f3eac21626794e90878c30a7e1b0a84e9dd03
MartinCespedes <martin.cespedes002@mymdc.net>
2023-01-11 20:08:35 -0500

    Added consts for output DIR & to generate html & finished managerQuestions array

--- index.js
+++ index.js
@@ -3,11 +3,34 @@ const fs = require("fs");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const teamMembersArray = [];
 
 const managerQuestions = [
   {
+    //Manager Name//
     type: "input",
     name: "managerName",
-    message: "what is your name? (Required)",
+    message: "What is your name? (Required)",
+  },
+  {
+    //Manager ID//
+    type: "input",
+    name: "managerEmployeeId",
+    message: "Enter your Employee ID",
+  },
+  {
+    //Manager Email//
+    type: "input",
+    name: "managerEmail",
+    message: "Enter your Email",
+  },
+  {
+    //Office Number//
+    type: "input",
+    name: "managerOfficeNumber",
+    message: "Enter your office number",
   },
 ];
